375b8a7ee209ba85686337071bbf8492e2f74055
id <dalyan.ismail@gmail.com>
2023-03-24 06:46:06 +0300

    4_Seed data  ve Context configurasyon ekleme yapıldı

--- Mevzuat6n.2Data/Context/AppDbContext.cs
+++ Mevzuat6n.2Data/Context/AppDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +11,17 @@ namespace Mevzuat6n._2Data.Context
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(){}
+        public AppDbContext() { }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
         public DbSet<Article> Articles { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Image> Images { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
