d33329823ed87b0dc41d637989f7b7a05d67727e
id <dalyan.ismail@gmail.com>
2023-03-25 10:21:12 +0300

    11.1_Identity için Entitiy'leri ekledim, Contexti değiştirdim

---
+++ Mevzuat6n.1Entities/Entities/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using Mevzuat6n._4Core.Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mevzuat6n._1Entities.Entities
+{
+    public class AppUser : IdentityUser<Guid>, IEntityBase
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Guid ImageId { get; set; } = Guid.Parse("4084c97a-2aa1-4675-b519-69f6fe410633");
+        public Image Image { get; set; }
+
+        public ICollection<Article> Articles { get; set; }
+    }
+}
--- Mevzuat6n.2Data/Context/AppDbContext.cs
+++ Mevzuat6n.2Data/Context/AppDbContext.cs
@@ -1,15 +1,11 @@
 ﻿using Mevzuat6n._1Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Mevzuat6n._2Data.Context
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext
     {
         public AppDbContext() { }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
